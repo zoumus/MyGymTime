1a5fd77097a3d7c1d8fde52c2c976460f6a3a829
zehour <zehourchaoui@gmail.com>
2023-06-11 22:54:28 -0700

    navbar

---
+++ MyGymTime/src/scenes/navbar/Link.tsx
@@ -0,0 +1,14 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+type Props = {
+    page: string;
+}
+
+const Link = (props: Props) => {
+  return (
+    <AnchorLink>
+        {}
+    </AnchorLink>
+  )
+}
+export default Link
--- MyGymTime/src/scenes/navbar/index.tsx
+++ MyGymTime/src/scenes/navbar/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from "react";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
+import logo from "@/assets/logo.png"
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  return (
-    <div>Navbar</div>
-  )
+    const flexBetween = "flex items-center justify-between";
+    return (
+        <nav>
+            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+                <div className={`${flexBetween} max-auto w-5/6`}>
+                    <div className={`${flexBetween} w-full gap-16`}>
+                        {/* left side */}
+                        <img alt="logo" src={logo} />
+                        {/* right side */}
+                        <div className={`${flexBetween} w-full`}>
+                            <div className={`${flexBetween} gap-8 text-sm`}>
+                                <p>Home</p>
+                                <p>Benefits</p>
+                                <p>Our Classes</p>
+                                <p>Contact Us</p>
+                            </div>
+                            <div className={`${flexBetween} gap-8`}>
+                                <p>Sign In</p>
+                                <button>Become a Member</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
+
+export default Navbar;
