4833285cad36e9eedb3476e91c95c41470d3cea9
zehour <zehourchaoui@gmail.com>
2023-07-18 18:50:53 -0700

    app.tsx

--- MyGymTime/src/App.tsx
+++ MyGymTime/src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
 
-function app() {
+function pp() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
