ad8e27d9dd927627e104debd71b4fc14d0150e8c
zehour <zehourchaoui@gmail.com>
2023-07-21 11:54:47 -0700

    app.tsx

--- MyGymTime/src/App.tsx
+++ MyGymTime/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
-    <div className="ap bg-grey-20">
+    <div className="ap bg-grey-50">
       <Navbar
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
